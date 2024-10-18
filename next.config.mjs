8f7c994f7c67013fe299000e0cd3c46037bcf8e7
uday pareta <udayparet028@gmail.com>
2024-10-18 20:13:54 +0530

    dfvf

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,7 @@ const nextConfig = {
       };
       return config;
     },
+    
   };
   
   
--- utils/database.js
+++ utils/database.js
@@ -2,26 +2,23 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
-export const connectToDB = async () =>{
-    mongoose.set('strictQuery' , true);
-    
-    if(isConnected){
-        console.log("Connected to mongodb");
-        return;
-    }
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);  // Enable strict query mode in Mongoose
 
-    try {
+  if (isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
 
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName:"share_prompt",
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-        })
-        isConnected = true;
-        console.log("connect to db")
-    } catch (error) {
-         console.log(error )
-    }
+  try {
+    // Connect to MongoDB using the connection string stored in environment variables
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",  // Specify the database name
+    });
 
-
-}
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
